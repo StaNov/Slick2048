0728242fbee1e27d0b9b7b5b745aa6fad105aa8d
stanov <stanovec@gmail.com>
2014-06-28 14:16:13 +0200

    text align and transparency

--- src/cz/stanov/slick2048/render/RenderConstants.java
+++ src/cz/stanov/slick2048/render/RenderConstants.java
@@ -4,4 +4,8 @@ package cz.stanov.slick2048.render;
 public class RenderConstants {
     public static final int TILE_SIZE_PIXELS = 100;
     public static final int TILE_MARGIN_PIXELS = 10;
+
+    public static final String FONT_NAME = "Time New Roman";
+    public static final int FONT_SIZE = 60;
+    public static final int TILE_TEXT_TRANSPARENCY = 200;
 }
--- src/cz/stanov/slick2048/render/TileRenderer.java
+++ src/cz/stanov/slick2048/render/TileRenderer.java
@@ -3,7 +3,9 @@ package cz.stanov.slick2048.render;
 import cz.stanov.slick2048.gameobject.Tile;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.TrueTypeFont;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,26 +13,52 @@ import static cz.stanov.slick2048.render.RenderConstants.*;
 
 public class TileRenderer {
 
+    // TODO vyhodit jinam
+    public static final Map<Integer, Color> colors = new HashMap<Integer, Color>();
+
     private Graphics g;
+    private TrueTypeFont tileTextFont;
+    private Color tileTextColor;
 
     public TileRenderer(Graphics g) {
         this.g = g;
+        this.tileTextFont = new TrueTypeFont(new Font(FONT_NAME, Font.BOLD, FONT_SIZE), true);
+        this.tileTextColor = new Color(0,0,0, TILE_TEXT_TRANSPARENCY);
     }
 
     public void renderTile(Tile tile, int x, int y) {
-        g.setColor(colors.get(tile.getValue()));
-        g.fillRect(x * TILE_SIZE_PIXELS + (x+1) * TILE_MARGIN_PIXELS,
-                y * TILE_SIZE_PIXELS + (y+1) * TILE_MARGIN_PIXELS,
-                TILE_SIZE_PIXELS, TILE_SIZE_PIXELS);
-
-        if (! tile.isEmpty()) {
-            g.setColor(Color.black);
-            g.drawString(String.valueOf(tile.getValue()), x* TILE_SIZE_PIXELS + (x+1)* TILE_MARGIN_PIXELS + 20, y* TILE_SIZE_PIXELS + (y+1)* TILE_MARGIN_PIXELS + 20);
-            // TODO zarovnat na střed
-        }
+        drawBackground(tile, x, y);
+        drawText(tile, x, y);
     }
 
-    private static Map<Integer, Color> colors = new HashMap<Integer, Color>();
+    private void drawBackground(Tile tile, int x, int y) {
+        g.setColor(colors.get(tile.getValue()));
+        g.fillRect(x * TILE_SIZE_PIXELS + (x+1) * TILE_MARGIN_PIXELS,
+                   y * TILE_SIZE_PIXELS + (y+1) * TILE_MARGIN_PIXELS,
+                   TILE_SIZE_PIXELS,
+                   TILE_SIZE_PIXELS);
+    }
+
+    private void drawText(Tile tile, int x, int y) {
+        if (tile.isEmpty()) {
+            // dont draw text of empty tile
+            return;
+        }
+
+        g.setColor(tileTextColor);
+        g.setFont(tileTextFont);
+        drawTileValue(tile, x, y);
+    }
+
+    private void drawTileValue(Tile tile, int x, int y) {
+        int valueTextWidth = g.getFont().getWidth(tile.getValue() + "");
+        int valueTextHeight = g.getFont().getHeight(tile.getValue() + "");
+
+        g.drawString(
+                tile.getValue() + "",
+                x* TILE_SIZE_PIXELS + (x+1)* TILE_MARGIN_PIXELS + (TILE_SIZE_PIXELS - valueTextWidth) / 2,
+                y* TILE_SIZE_PIXELS + (y+1)* TILE_MARGIN_PIXELS + (TILE_SIZE_PIXELS - valueTextHeight) / 2);
+    }
 
     static {
         colors.put(0, Color.gray);
