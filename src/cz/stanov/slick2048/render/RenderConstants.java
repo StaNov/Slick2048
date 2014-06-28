2b23201f4d52060993704d1f932f05e0293bb3df
stanov <stanovec@gmail.com>
2014-06-28 16:48:50 +0200

    tile colors

--- src/cz/stanov/slick2048/render/RenderConstants.java
+++ src/cz/stanov/slick2048/render/RenderConstants.java
@@ -7,28 +7,28 @@ public class RenderConstants {
     public static final int TILE_SIZE_PIXELS = 100;
     public static final int TILE_MARGIN_PIXELS = 10;
 
-    public static final String FONT_NAME = "Verdana";
-    public static final int TILE_TEXT_TRANSPARENCY = 200;
+    public static final String FONT_NAME = "Verdana"; // TODO načítat font, teď je tam ten původní
+    public static final int TILE_TEXT_TRANSPARENCY = 180;
 
     public static final Color BACKGROUND_COLOR = Color.darkGray;
     
     public static final Color TILE_COLOR_EMPTY = Color.gray;
-    public static final Color TILE_COLOR_2 = Color.blue;
-    public static final Color TILE_COLOR_4 = Color.green;
-    public static final Color TILE_COLOR_8 = Color.pink;
-    public static final Color TILE_COLOR_16 = Color.red;
-    public static final Color TILE_COLOR_32 = Color.lightGray;
-    public static final Color TILE_COLOR_64 = Color.cyan;
-    public static final Color TILE_COLOR_128 = Color.magenta;
-    public static final Color TILE_COLOR_256 = Color.orange;
-    public static final Color TILE_COLOR_512 = Color.yellow;
-    public static final Color TILE_COLOR_1024 = Color.white;
-    public static final Color TILE_COLOR_2048 = Color.blue;
-    public static final Color TILE_COLOR_OTHER = Color.red;
+    public static final Color TILE_COLOR_2 = Color.decode("#D4D971");
+    public static final Color TILE_COLOR_4 = Color.decode("#86D971");
+    public static final Color TILE_COLOR_8 = Color.decode("#71D9C0");
+    public static final Color TILE_COLOR_16 = Color.decode("#71BFD9");
+    public static final Color TILE_COLOR_32 = Color.decode("#7171D9");
+    public static final Color TILE_COLOR_64 = Color.decode("#B671D9");
+    public static final Color TILE_COLOR_128 = Color.decode("#D971B8");
+    public static final Color TILE_COLOR_256 = Color.decode("#D97171");
+    public static final Color TILE_COLOR_512 = Color.decode("#D9A671");
+    public static final Color TILE_COLOR_1024 = Color.decode("#35D420");
+    public static final Color TILE_COLOR_2048 = Color.decode("#F22929");
+    public static final Color TILE_COLOR_OTHER = Color.decode("#FFF700");
 
     public static final int TILE_FONT_SIZE_1_DIGIT = 70;
     public static final int TILE_FONT_SIZE_2_DIGITS = 60;
     public static final int TILE_FONT_SIZE_3_DIGITS = 40;
-    public static final int TILE_FONT_SIZE_4_DIGITS = 20;
+    public static final int TILE_FONT_SIZE_4_DIGITS = 30;
     public static final int TILE_FONT_SIZE_OTHER = 20;
 }
